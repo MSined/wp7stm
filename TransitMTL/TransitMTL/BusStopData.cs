977cd610d983689993825c09c9029cf0bc2d0243
denismaniti <denismaniti@40f10821-3fef-d23d-3bf9-054c0039867f>
2012-04-28 19:14:07 +0000

    Another Big Update (Formatting just about complete)

---
+++ TransitMTL/TransitMTL/BusStopData.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Collections.Generic;
+
+namespace TransitMTL
+{
+    public class BusStopData
+    {
+        String stopNumber;
+        String direction;
+        List<String> times;
+
+        public BusStopData(String stopNumber, String direction, List<String> times)
+        {
+            this.stopNumber = stopNumber;
+            this.direction = direction;
+            this.times = times;
+        }
+
+        public String getStopNumber()
+        {
+            return stopNumber;
+        }
+
+        public String getDirection()
+        {
+            return direction;
+        }
+
+        public List<String> getTimes()
+        {
+            return times;
+        }
+    }
+}
