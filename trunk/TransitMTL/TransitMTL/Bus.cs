8d35d5ee3089cb380674f79a8432d9b1a8801f4f
denismaniti <denismaniti@40f10821-3fef-d23d-3bf9-054c0039867f>
2012-05-01 03:00:30 +0000

    Bus Lines now added dynamically

---
+++ trunk/TransitMTL/TransitMTL/Bus.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace TransitMTL
+{
+    public class Bus
+    {
+        string busNumber;
+        string busName;
+        public Bus(string busNumber, string busName)
+        {
+            this.busNumber = busNumber;
+            this.busName = busName;
+        }
+
+        public string getBusNumber()
+        {
+            return busNumber;
+        }
+
+        public string getBusName()
+        {
+            return busName;
+        }
+    }
+}
